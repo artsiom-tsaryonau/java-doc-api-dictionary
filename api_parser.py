4b4b92ed8ecb82f85fa9f7e4a972d7d258c4487f
artsiom-tsaryonau <artemii1992@gmail.com>
2019-10-31 12:44:25 +0100

    Refactoring

--- api_parser.py
+++ api_parser.py
@@ -2,71 +2,120 @@ from bs4 import BeautifulSoup
 from multiprocessing import Pool
 import requests, csv, re
 
-base_url = 'https://docs.oracle.com/en/java/javase/11/docs/api/'
-modules_file = 'java11_modules.csv'
+BASE_URL = 'https://docs.oracle.com/en/java/javase/11/docs/api/'
+MODULES_FILE_LIST_NAME = 'java11_modules.csv'
+MODULE_FILE_NAME_POSTFIX = '_module.csv'
+ALLOWED_MODULE = 'java'
+MODULES_TABLE_NAME = 'overviewSummary'
 
-# indirect exports might be convenient if we were to build 
-# true graph of modules and its packages
+def normalize_text(text):
+    return re.sub(r'\n+', '', text)
+    
+def create_direct_link(relative_link):
+    return BASE_URL + relative_link
+ 
+# parses single entry in module table
+def parse_jdk_module_entry(entry):
+    header = entry.find('th')
+    content = entry.find('td')
+            
+    entry_name = header.text
+    entry_relative_link = header.find('a').get('href')
+    entry_link = create_direct_link(entry_relative_link)
+    entry_description = normalize_text(content.text)
 
-# hierarchy: 
-# module -> 
-#   package(exports, indirect exports)
-#       interface, class, enum, exception
-#   service(provides, uses)
-#   modules(requires, indirect requires)
+    return [entry_name, entry_link, entry_description]
+        
+# perform parallel processing of table entries
+def parse_jdk_module_overview_table(summary_table):
+    overview_table = summary_table.find('tbody')
+    list_result = []
+    for entry in overview_table.find_all('tr'):
+        list_result.append(parse_jdk_module_entry(entry))
+    return list_result
 
-# parses main index.html page
-def parse_main_doc():
-    url = base_url + 'index.html'
+# 1. parse JDK module list
+def parse_jdk_modules_page():
+    url = BASE_URL + 'index.html'
     html = requests.get(url).text
     soup = BeautifulSoup(html, features='html.parser')
-    modules_dict = {} # modules for further processing
-    with open(modules_file, mode='w') as modules_export:
-        modules_writer = csv.writer(modules_export, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        overviewtable = soup.body.find('table', attrs={'class':'overviewSummary'})
-        modules = overviewtable.find('tbody')
-        for row in modules.find_all('tr'):
-            header = row.find('th')
-            module_name = header.text
-            module_link = base_url + header.find('a').get('href')
-            description_raw = row.find('td').text
-            description = re.sub(r'\n+', '', description_raw)
+    jdk_modules_dict = {}
+    with open(MODULES_FILE_LIST_NAME, mode='w') as jdk_modules_file:
+        jdk_modules_writer = csv.writer(jdk_modules_file, 
+                                        delimiter=',', 
+                                        quotechar='"', 
+                                        quoting=csv.QUOTE_MINIMAL)
+        summary_table = soup.body.find('table', 
+                                       attrs={'class':MODULES_TABLE_NAME})
+                                       
+        def process_result_funct(list_result_entry):
+            if list_result_entry[0].startswith(ALLOWED_MODULE):
+                jdk_modules_writer.writerow(list_result_entry)
+                jdk_modules_dict[list_result_entry[0]] = list_result_entry[1]
+                
+        list_result = parse_jdk_module_overview_table(summary_table)
+        for module_info in list_result:
+            process_result_funct(module_info)
         
-            if module_name.startswith('java'):
-                row = [module_name, module_link, description]
-                modules_writer.writerow(row)
-                modules_dict[module_name] = module_link
-    return modules_dict
-    
-def parse_module_doc(module_pair):
-    print(module_pair[0] + ' -> ' + module_pair[1])
-    html = requests.get(module_pair[1]).text
-    soup = BeautifulSoup(html, features='html.parser')
-    module_filename = module_pair[0] + '.csv'
-    with open(module_filename, mode='w') as modules_content:
-        modules_content_writer = csv.writer(modules_content, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        # have packages
-        packagestable = soup.body.find('table', attrs={'class':'packagesSummary'})
-        packages = packagestable.find('tbody')
-        for row in packages.find_all('tr'):
-            header = row.find('th')
-            package_name = header.text
-            package_link = base_url + header.find('a').get('href')
-            description_raw = row.find('td').text
-            description = re.sub(r'\n+', '', description_raw)
-            
-            print (package_name + ' -> ' + package_link)
-            row = [package_name, package_link, description]
-            modules_content_writer.writerow(row)
-            
-        # have services which are classes
-    print('\n')
-    
-# probably can be parallelized    
-def parse_modules(modules_dict):
-    with Pool(4) as p:
-        p.map(parse_module_doc, modules_dict.items())
+    return jdk_modules_dict
 
-modules_dict = parse_main_doc()
-parse_modules(modules_dict)
+# 2. parse JDK module
+def parse_jdk_module_page(module_name, module_page_link):
+    html = requests.get(module_page_link).text
+    soup = BeautifulSoup(html, features='html.parser')
+    filename = module_name + MODULE_FILE_NAME_POSTFIX
+    with open(filename, mode='w') as module_content:
+        module_content_writer = csv.writer(module_content, 
+                                           delimiter=',', 
+                                           quotechar='"', 
+                                           quoting=csv.QUOTE_MINIMAL)
+        # check if has <a id='packages.summary'>
+        # can be multiple - difference in caption
+        packages = soup.body.find_all('table', 
+                                  attrs={'class':'packagesSummary'})
+        process_packages(packages, module_content_writer)
+        # check if has <a id='services.summary'>
+        provides = soup.body.find('table',
+                                  attrs={'class':'providesSummary'})
+        uses = soup.body.find('table',
+                              attrs={'class':'usesSummary'})
+        # check if has <a id='modules.summary'>
+        # can be multiple - difference in caption
+        requires = soup.body.find_all('table',
+                                  attrs={'class':'requiresSummary'})
+    
+def process_packages(packages, csv_writer):
+    package_lists = parkse_jdk_module_packages(packages)
+    for package_list in package_lists:
+        for package_entry in package_list:
+            csv_writer.writerow(package_entry)
+                
+def parkse_jdk_module_packages(packages):
+    package_lists = []
+    for package_table in packages:
+        list_result = parse_jdk_module_package(package_table)
+        package_lists.append(list_result)
+    return package_lists
+    
+def parse_jdk_module_package(package):
+    list_result = []
+    for row in package.find_all('tr'):
+        if row.find('th').text != 'Package':
+            list_result.append(parse_jdk_module_package_entry(row))
+    return list_result
         
+def parse_jdk_module_package_entry(entry):
+    header = entry.find('th')
+    content = entry.find('td')
+        
+    entry_name = header.text
+    entry_relative_link = header.find('a').get('href')
+    entry_link = create_direct_link(entry_relative_link)
+    entry_description = normalize_text(content.text)
+    return [entry_name, entry_link, entry_description]
+
+# exec
+modules_dict = parse_jdk_modules_page()
+for key, value in modules_dict.items():
+    parse_jdk_module_page(key, value)
+    break
